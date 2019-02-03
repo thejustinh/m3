4d674535f43bcb86c0f8a6428ae5ac054a47641b
Justin Kyle Herrera <jherre34@pi01.csc.calpoly.edu>
2019-02-03 02:43:43 +0000

    added addwithprint.c

--- addWithPrint/addwithprint.s
+++ addWithPrint/addwithprint.s
@@ -9,7 +9,7 @@
 	.eabi_attribute 30, 6
 	.eabi_attribute 34, 1
 	.eabi_attribute 18, 4
-	.file	"fib.c"
+	.file	"addwithprint.c"
 	.text
 	.align	2
 	.global	main
@@ -18,43 +18,27 @@
 	.fpu vfp
 	.type	main, %function
 main:
-	@ args = 0, pretend = 0, frame = 16
+	@ args = 0, pretend = 0, frame = 8
 	@ frame_needed = 1, uses_anonymous_args = 0
-	@ link register save eliminated.
-	str	fp, [sp, #-4]!
-	add	fp, sp, #0
-	sub	sp, sp, #20
-	mov	r3, #0
+	push	{fp, lr}
+	add	fp, sp, #4
+	sub	sp, sp, #8
+	mov	r3, #10
 	str	r3, [fp, #-8]
-	mov	r3, #1
+	mov	r3, #200
 	str	r3, [fp, #-12]
-	mov	r3, #0
-	str	r3, [fp, #-16]
-	mov	r3, #0
-	str	r3, [fp, #-20]
-	b	.L2
-.L3:
 	ldr	r2, [fp, #-8]
 	ldr	r3, [fp, #-12]
 	add	r3, r2, r3
-	str	r3, [fp, #-16]
-	ldr	r3, [fp, #-12]
-	str	r3, [fp, #-8]
-	ldr	r3, [fp, #-16]
-	str	r3, [fp, #-12]
-	ldr	r3, [fp, #-20]
-	add	r3, r3, #1
-	str	r3, [fp, #-20]
-.L2:
-	ldr	r3, [fp, #-20]
-	cmp	r3, #9
-	ble	.L3
-	ldr	r3, [fp, #-16]
 	mov	r0, r3
-	add	sp, fp, #0
+	bl	printint
+	ldr	r2, [fp, #-8]
+	ldr	r3, [fp, #-12]
+	add	r3, r2, r3
+	mov	r0, r3
+	sub	sp, fp, #4
 	@ sp needed
-	ldr	fp, [sp], #4
-	bx	lr
+	pop	{fp, pc}
 	.size	main, .-main
 	.ident	"GCC: (Raspbian 6.3.0-18+rpi1+deb9u1) 6.3.0 20170516"
 	.section	.note.GNU-stack,"",%progbits
